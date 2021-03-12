0ede3afe623a7b829cf4079f751d7a5e36aa3ea2
vkononiuk <vkononiuk@student.ucode.world>
2021-03-12 21:56:25 +0200

    Added items cart

--- js/products.js
+++ js/products.js
@@ -2,20 +2,20 @@ class Products {
 
     render() {
         let htmlCatalog = "";
-        CATALOG.forEach(({id, name, price, img}) => {
+        CATALOG.forEach(({id, name, price, img, info}) => {
 
             htmlCatalog += `
                 <div class="service_section_box">
                     <div class="bc_color"></div>
                     <div class="service_image">
-                        <img src="${img}" alt="pic" /
+                        <img src="${img}" alt="pic">
                     </div>
                     <div class="txt">
                         <span class="service_name">${name}</span>
-<!--                        <br>-->
-                        <span class="service_info"></span>
+                        <br>
+                        <span class="service_info">${info}</span>
                         <div class="service_price">
-                            <span class="cost">Вартість - ${price}</span>
+                            <span>вартість - <span class="cost">${price} грн</span></span>
                         </div>
                     </div>
                 </div>
